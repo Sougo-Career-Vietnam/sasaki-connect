0a67584488369c8511dcee4fab12b279efe961dc
Duylinh <duylinh87.wru@gmail.com>
2025-04-09 15:14:26 +0700

    do top

--- resources/views/components/topContent.blade.php
+++ resources/views/components/topContent.blade.php
@@ -30,6 +30,71 @@
                  </div>
 
             </section>
+
+            <section class="block-main">
+                <div class="inner">
+                    <h4 class="title-page"><span>SASAKI CONNECTのサービス</span></h4>
+
+                    <ul class="list-connect">
+                         <li class="item-conncet flex">
+                            <dl class="dl-conncet">
+                                <dt class="dt-conncet">サービス紹介<span class="es-dt">SERVICE</span></dt>
+                                <dd class="dd-conncet">人材不足の「困った」を当社４つのソリューションをベースにさまざまな角度からアプローチ。特に半導体製造、航空・宇宙・防衛関連、医療機器製造の各業種を得意としております。</dd>
+
+                                <a class="link-top two" href="">サービス紹介ページへ</a>
+                            </dl>
+                            <div class="box-img">
+                                <picture>
+                                    <source media="(max-width: 767px)" srcset="{{asset('images/top/top_img_02_sp.png')}} 2x">
+                                    <source media="(min-width: 768px)" srcset="{{asset('images/top/top_img_02_pc.png')}} 2x">
+                                    <img class="sizes" src="{{asset('images/top/top_img_02_pc.png')}}" alt="">
+                                </picture> 
+                            </div>
+                         </li>
+                         <li class="item-conncet flex">
+                            <dl class="dl-conncet">
+                                <dt class="dt-conncet">選ばれる理由<span class="es-dt">REASON</span></dt>
+                                <dd class="dd-conncet">当社はすでに大手企業から地場企業まで製造業の企業様にご愛顧いただいております。<br>お客様、スタッフ様、そして当社の三方がWin-Winになる「三方良しの好循環」が私たちの強み・選ばれる理由です。</dd>
+
+                                <a class="link-top two" href="">選ばれる理由ページへ</a>
+                            </dl>
+                            <div class="box-img">
+                                <picture>
+                                    <source media="(max-width: 767px)" srcset="{{asset('images/top/top_img_03_sp.png')}} 2x">
+                                    <source media="(min-width: 768px)" srcset="{{asset('images/top/top_img_03_pc.png')}} 2x">
+                                    <img class="sizes" src="{{asset('images/top/top_img_03_pc.png')}}" alt="">
+                                </picture> 
+                            </div>
+                         </li>
+
+                         <li class="item-conncet flex">
+                            <dl class="dl-conncet">
+                                <dt class="dt-conncet">ご利用の流れ<span class="es-dt">FLOW</span></dt>
+                                <dd class="dd-conncet">「人材不足で困っている」「初めて問い合わせたい」という方に、当社サービスをご利用いただくまでの流れを記しました。<br>お気軽にお問い合わせください。</dd>
+
+                                <a class="link-top two" href="">ご利用の流れページへ</a>
+                            </dl>
+                            <div class="box-img">
+                                <picture>
+                                    <source media="(max-width: 767px)" srcset="{{asset('images/top/top_img_04_sp.png')}} 2x">
+                                    <source media="(min-width: 768px)" srcset="{{asset('images/top/top_img_04_pc.png')}} 2x">
+                                    <img class="sizes" src="{{asset('images/top/top_img_04_pc.png')}}" alt="">
+                                </picture> 
+                            </div>
+                         </li>
+
+
+
+
+
+                    </ul>
+
+
+                </div>
+
+
+
+            </section>
    
 
 
