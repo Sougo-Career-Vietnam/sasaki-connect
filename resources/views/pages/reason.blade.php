5786818083bbb8215fe5b83ca87d3e7b0cc4bb57
Duylinh <duylinh87.wru@gmail.com>
2025-04-22 14:37:18 +0700

    addallpage

--- resources/views/pages/reason.blade.php
+++ resources/views/pages/reason.blade.php
@@ -7,32 +7,140 @@
     <div id="contentArea">
 
         {{--html埋め込み部分 start--}}
-      
-        <link rel="stylesheet" media="all" href="./css/reason.css"/>
         <link rel="stylesheet" media="all" href="./css/top.css"/>
+        <link rel="stylesheet" media="all" href="./css/reasons.css"/>
         <main id="main_wrap">
 
         
-          <div id="recruit">
-            <!-- banner-page -->
-            <x-titlePage />
-
-               <!-- content-main -->
-                <div class="content-main">
-                    <!-- Career move-->
-
+            <div id="reasons" class="reasons">
+                <!-- strengths -->
+                <section id="sc-strengths">
+                    <div class="inner">
+                        <div class="strengths-inner">
+                            <h2 class="section-title">
+                                <img class="sizes" src="{{asset('images/reasons/reasons_title.png')}}" alt="">
+                                私たちの強み
+                            </h2>
+                            <div class="coming-soon">制作中</div>
+                            <p class="text">
+                                私たちは「人が現場を強くして、経営も強くする」というコンセプトを掲げております。<br />
+                                お客様・スタッフ様・私たちの三方それぞれ良い結果になるよう、三方の好循環を作り上げることによって、スタッフ様には高い満足度と十分な還元を果たし、お客様には生産性・生産力が向上して生産活動が活発化し、企業経営を強くすることを目指します。<br />
+                                人も生産活動も健康体の血液のような好循環を実現し、DNAのように人と技術の双方から日本の製造業を足元から強くしていくことを目指して、人材サービスとして私たちSASAKI CONNECTが尽力いたします。
+                            </p>
+                        </div>
+                    </div>
+                </section>
     
     
+                <!-- benefits -->
+                <section id="sc-benefits">
+                    <div class="inner">
+                        <div class="benefits-inner">
+                            <h2 class="section-title">
+                                <img class="sizes" src="{{asset('images/reasons/reasons_title.png')}}" alt="">
+                                選ばれる理由
+                            </h2>
+                            <div class="list">
+                                <div class="group">
+                                    <div class="group-head">
+                                        <div class="circle">理由<br />
+                                            <span class="small">その</span>
+                                            <span class="num">1</span>
+                                        </div>
+                                        <h3 class="group-title">自社工場を持つゆえに製造現場に強く、<br class="sp-br" />製造現場の悩みに直接お応えする</h3>
+                                    </div>
+                                    <div class="img">
+                                        <picture>
+                                            <source media="(max-width: 767px)" srcset="{{asset('images/reasons/reasons_benefits_01_sp.jpg')}}">
+                                            <source media="(min-width: 768px)" srcset="{{asset('images/reasons/reasons_benefits_01_pc.jpg')}}">
+                                            <img class="sizes" src="{{asset('images/reasons/reasons_benefits_01_pc.jpg')}}" alt="">
+                                        </picture>
+                                    </div>
+                                    <p class="text">
+                                        弊社はハーネス製造のササキのグループ会社です。本社はササキ自社工場に直結しており、製造現場の状況を熟知しております。<br />
+                                        製造現場をもつ強みを活かして、製造現場におけるボトルネックや人材不足などにおける課題やニーズへのキャッチなど「現場感」の課題を深く理解することができます。それゆえ製造現場へのお悩みに直接お応えができる提案ができるのです。
+                                    </p>
+                                </div>
+                                <div class="group">
+                                    <div class="group-head">
+                                        <div class="circle">理由<br />
+                                            <span class="small">その</span>
+                                            <span class="num">2</span>
+                                        </div>
+                                        <h3 class="group-title">半導体や医療機器、航空宇宙防衛産業な<br class="sp-br" />ど成長事業・産業に特に強い</h3>
+                                    </div>
+                                    <div class="img">
+                                        <picture>
+                                            <source media="(max-width: 767px)" srcset="{{asset('images/reasons/reasons_benefits_02_sp.jpg')}}">
+                                            <source media="(min-width: 768px)" srcset="{{asset('images/reasons/reasons_benefits_02_pc.jpg')}}">
+                                            <img class="sizes" src="{{asset('images/reasons/reasons_benefits_02_pc.jpg')}}" alt="">
+                                        </picture>
+                                    </div>
+                                    <p class="text">
+                                        弊社は製造業の顧客様への人材サービスに特化しておりますが、なかでも成長著しい半導体製造・組み立て関連や、医療機器製造、航空宇宙・防衛産業への製造業は特に強く、これらの成長事業・産業に対してより細やかなご提案をいたします。人材ニーズとしてもスキルアップやキャリアのステップアップを望む人材が多いことから双方のニーズをコネクトすることを得意とする会社であります。
+                                    </p>
+                                </div>
+                                <div class="group">
+                                    <div class="group-head">
+                                        <div class="circle">理由<br />
+                                            <span class="small">その</span>
+                                            <span class="num">3</span>
+                                        </div>
+                                        <h3 class="group-title">「強い人材・労働力」の提供及び支援</h3>
+                                    </div>
+                                    <div class="img">
+                                        <picture>
+                                            <source media="(max-width: 767px)" srcset="{{asset('images/reasons/reasons_benefits_03_sp.jpg')}}">
+                                            <source media="(min-width: 768px)" srcset="{{asset('images/reasons/reasons_benefits_03_pc.jpg')}}">
+                                            <img class="sizes" src="{{asset('images/reasons/reasons_benefits_03_pc.jpg')}}" alt="">
+                                        </picture>
+                                    </div>
+                                    <p class="text">
+                                        人材サービスと言っても、誰でもよいというわけではございません。貴社の現場に合わせたスキル、マインドなど必要不可欠な部分がございます。たとえ未経験であっても製造業についての基礎知識、スキルを自社で研修し、お客様の現場へとお繋ぎすることができます。これは実際に製造現場を持つ弊社だからこその強みゆえであり、よりお客様のご要望にあわせた「強い」人材提供が可能になります。
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
     
     
-     
-    
-    
-    
-      
-                </div>
-        
-           </div>  
+                <!-- feedbacks -->
+                <section id="sc-feedbacks">
+                    <div class="line line1">
+                        <picture>
+                            <source media="(max-width: 767px)" srcset="{{asset('images/reasons/reasons_feedback_sp.png')}}">
+                            <source media="(min-width: 768px)" srcset="{{asset('images/reasons/reasons_feedback_pc.png')}}">
+                            <img class="sizes" src="{{asset('images/reasons/reasons_feedback_pc.png')}}" alt="">
+                        </picture>
+                    </div>
+                    <div class="line line2">
+                        <picture>
+                            <source media="(max-width: 767px)" srcset="{{asset('images/reasons/reasons_feedback_sp.png')}}">
+                            <source media="(min-width: 768px)" srcset="{{asset('images/reasons/reasons_feedback_pc.png')}}">
+                            <img class="sizes" src="{{asset('images/reasons/reasons_feedback_pc.png')}}" alt="">
+                        </picture>
+                    </div>
+                    <div class="inner">
+                        <div class="feedbacks-inner">
+                            <h2 class="feedbacks-title">
+                                <img class="sizes" src="{{asset('images/reasons/reasons_title.png')}}" alt="">
+                                お客様の声
+                            </h2>
+                            <div class="list">
+                                <div class="box">
+                                    <h3 class="box-title">製造業TE社（長野県）採用担当T様</h3>
+                                    <p class="text">テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります</p>
+                                </div>
+                                <div class="box">
+                                    <h3 class="box-title">製造業P社（山梨県）工場長K様</h3>
+                                    <p class="text">テキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入りますテキストが入ります</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div> 
            <x-contactFooter />           
           
 
--- resources/views/pages/service.blade.php
+++ resources/views/pages/service.blade.php
@@ -8,30 +8,304 @@
 
         {{--html埋め込み部分 start--}}
       
-        <link rel="stylesheet" media="all" href="./css/reason.css"/>
+        <link rel="stylesheet" media="all" href="./css/service.css"/>
         <link rel="stylesheet" media="all" href="./css/top.css"/>
         <main id="main_wrap">
 
             <x-titlePage />
-          <div id="service">
-            <!-- banner-page -->
 
-               <!-- content-main -->
-                <div class="content-main">
-                    <!-- Career move-->
-
-    
-    
-    
-    
-     
-    
-    
-    
-      
+           
+        <div id="service" class="service">
+            <!-- endpoint -->
+            <div id="sc-endpoint">
+                <div class="inner">
+                    <div class="endpoint-inner">
+                        <a href="#sc-manufacture" class="link scroll">製造業特化型人材サービス</a>
+                        <a href="#sc-seekers" class="link scroll">求職者向けお仕事紹介サービス</a>
+                    </div>
                 </div>
-        
-            </div>         
+            </div>
+
+            <!-- manufacture -->
+            <section id="sc-manufacture">
+                <div class="inner">
+                    <h2 class="section-title">
+                        <img class="sizes" src="{{asset('images/service/service_title.png')}}" alt="">
+                        製造業特化型人材サービス
+                    </h2>
+                    <div class="box">
+                        <h3 class="box-title">こんなお悩みはありませんか？</h3>
+                        <div class="img">
+                            <picture>
+                                <source media="(max-width: 767px)" srcset="{{asset('images/service/service_manufacture_sp.png')}}">
+                                <source media="(min-width: 768px)" srcset="{{asset('images/service/service_manufacture_pc.png')}}">
+                                <img class="sizes" src="{{asset('images/service/service_manufacture_pc.png')}}" alt="">
+                            </picture>
+                        </div>
+                        <ul class="list">
+                            <li class="text">人材不足で困っている</li>
+                            <li class="text">採用ができず応募が来ない</li>
+                            <li class="text">スタッフが定着しない</li>
+                            <li class="text">増産のため現場スタッフを増員したい</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+
+            <!-- solutions -->
+            <section id="sc-solutions">
+                <div class="inner">
+                    <div class="solutions-inner">
+                        <div class="content">
+                            <h3 class="block-title">当社４つのソリューション</h3>
+                            <p class="text-bold">人材不足の「困った」を<br />さまざまな角度からアプローチします</p>
+                            <p class="text">
+                                私たちは「人が現場を強くして、経営も強くする」というコンセプトを掲げております。<br />
+                                SASAKI CONNECTでは人材に対するお困りに対し、企業様それぞれの背景を詳しく伺いまして、「人材派遣」「人材紹介」「製造請負」「採用コンサルティング」の４つの面からアプローチいたします。特に中長期的な人材育成面を鑑みて、人材紹介は私たちSASAKI CONNECTが最も得意とするところです。
+                            </p>
+                        </div>
+                        <div class="coming-soon">制作中</div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- problems -->
+            <section id="sc-problems">
+                <div class="problems-inner">
+                    <h3 class="title">こういった「困った」に全力でお応えします</h3>
+                    <div class="list">
+                        <p class="text">人事・労務の手間や<br />固定費を削減したい</p>
+                        <p class="text">繁忙期に増員が必要</p>
+                        <p class="text">特定の業務のみを<br />委託したい</p>
+                        <p class="text">新規採用に不安がある</p>
+                        <p class="text">専門職のスキルを<br />持った人材が必要</p>
+                        <p class="text">技術者養成の時間と<br />経費がない</p>
+                        <p class="text text-fit">生産設備の稼働率を<br />高めたい・扱う人がいない</p>
+                    </div>
+                    <div class="img">
+                        <picture>
+                            <source media="(max-width: 767px)" srcset="{{asset('images/service/service_problems_sp.png')}}">
+                            <source media="(min-width: 768px)" srcset="{{asset('images/service/service_problems_pc.png')}}">
+                            <img class="sizes" src="{{asset('images/service/service_problems_pc.png')}}" alt="">
+                        </picture>
+                    </div>
+                </div>
+            </section>
+
+            <!-- highlights -->
+            <section id="sc-highlights">
+                <div class="inner">
+                    <h3 class="block-title">サービスのポイント</h3>
+                    <ul class="list">
+                        <li class="text">現場レベルのお悩みから、経営改善における人材戦略まで対応いたします</li>
+                        <li class="text">高度技術・成長産業は特に得意としており、特化した人材戦略をご提供いたします</li>
+                        <li class="text">採用力や人材育成、請負までお客様の製造現場に合わせたご提案をいたします</li>
+                        <li class="text">お客様の課題に対して親身になって伺い、柔軟に考えてご提案いたします</li>
+                    </ul>
+                    <a href="#" class="link">選ばれる理由ページへ</a>
+                </div>
+            </section>
+
+            <!-- particular -->
+            <section id="sc-particular">
+                <div class="inner">
+                    <h3 class="block-title">特に強い業界・分野・職種</h3>
+                    <div class="industries">
+                        <h4 class="title">◆ &nbsp;業界</h4>
+                        <div class="list">
+                            <div class="item">
+                                <p class="text">半導体製造</p>
+                                <div class="img">
+                                    <picture>
+                                        <source media="(max-width: 767px)" srcset="{{asset('images/service/service_particular_01_sp.jpg')}}">
+                                        <source media="(min-width: 768px)" srcset="{{asset('images/service/service_particular_01_pc.jpg')}}">
+                                        <img class="sizes" src="{{asset('images/service/service_particular_01_pc.jpg')}}" alt="">
+                                    </picture>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <p class="text">航空・宇宙・防衛関連</p>
+                                <div class="img">
+                                    <picture>
+                                        <source media="(max-width: 767px)" srcset="{{asset('images/service/service_particular_02_sp.jpg')}}">
+                                        <source media="(min-width: 768px)" srcset="{{asset('images/service/service_particular_02_pc.jpg')}}">
+                                        <img class="sizes" src="{{asset('images/service/service_particular_02_pc.jpg')}}" alt="">
+                                    </picture>
+                                </div>
+                            </div>
+                            <div class="item">
+                                <p class="text">医療機器製造</p>
+                                <div class="img">
+                                    <picture>
+                                        <source media="(max-width: 767px)" srcset="{{asset('images/service/service_particular_03_sp.jpg')}}">
+                                        <source media="(min-width: 768px)" srcset="{{asset('images/service/service_particular_03_pc.jpg')}}">
+                                        <img class="sizes" src="{{asset('images/service/service_particular_03_pc.jpg')}}" alt="">
+                                    </picture>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="fields">
+                        <h4 class="title">◆ &nbsp;分野</h4>
+                        <div class="list">
+                            <div class="item item1">
+                                <div class="img">
+                                    <picture>
+                                        <source srcset="{{asset('images/service/service_particular_icon_01.png')}}">
+                                        <img class="sizes" src="{{asset('images/service/service_particular_icon_01.png')}}" alt="">
+                                    </picture>
+                                </div>
+                                <p class="text">生産技術</p>
+                            </div>
+                            <div class="item item2">
+                                <div class="img">
+                                    <picture>
+                                        <source srcset="{{asset('images/service/service_particular_icon_02.png')}}">
+                                        <img class="sizes" src="{{asset('images/service/service_particular_icon_02.png')}}" alt="">
+                                    </picture>
+                                </div>
+                                <p class="text">開発</p>
+                            </div>
+                            <div class="item item3">
+                                <div class="img">
+                                    <picture>
+                                        <source srcset="{{asset('images/service/service_particular_icon_03.png')}}">
+                                        <img class="sizes" src="{{asset('images/service/service_particular_icon_03.png')}}" alt="">
+                                    </picture>
+                                </div>
+                                <p class="text">製造</p>
+                            </div>
+                            <div class="item item4">
+                                <div class="img">
+                                    <picture>
+                                        <source srcset="{{asset('images/service/service_particular_icon_04.png')}}">
+                                        <img class="sizes" src="{{asset('images/service/service_particular_icon_04.png')}}" alt="">
+                                    </picture>
+                                </div>
+                                <p class="text">電気設計</p>
+                            </div>
+                            <div class="item item5">
+                                <div class="img">
+                                    <picture>
+                                        <source srcset="{{asset('images/service/service_particular_icon_05.png')}}">
+                                        <img class="sizes" src="{{asset('images/service/service_particular_icon_05.png')}}" alt="">
+                                    </picture>
+                                </div>
+                                <p class="text">機械設計</p>
+                            </div>
+                            <div class="item item6">
+                                <div class="img">
+                                    <picture>
+                                        <source srcset="{{asset('images/service/service_particular_icon_06.png')}}">
+                                        <img class="sizes" src="{{asset('images/service/service_particular_icon_06.png')}}" alt="">
+                                    </picture>
+                                </div>
+                                <p class="text">物流</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="occupations">
+                        <h4 class="title">◆ &nbsp;職種</h4>
+                        <div class="list">
+                            <p class="text">●ハーネス加工</p>
+                            <p class="text">●CADオペレーター</p>
+                            <p class="text">●プロセス技術</p>
+                            <p class="text">●生産管理</p>
+                            <p class="text">●現地スタートアップ</p>
+                            <p class="text">●現地検査・調整</p>
+                            <p class="text">●装置設計</p>
+                            <p class="text">●装置組立 &nbsp;ほか</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <!-- contact -->
+            <section id="sc-contact">
+                <div class="inner">
+                    <div class="contact-inner">
+                        <div class="img">
+                            <picture>
+                                <source srcset="{{asset('images/service/service_contact.jpg')}}">
+                                <img class="sizes" src="{{asset('images/service/service_contact.jpg')}}" alt="">
+                            </picture>
+                        </div>
+                        <dl class="content">
+                            <dt class="title">私たちは特に０００００に力を入れております</dt>
+                            <dd class="text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</dd>
+                        </dl>
+                    </div>
+                    <a href="#" class="link">お問い合わせ・ご相談はこちら</a>
+                </div>
+            </section>
+
+            <!-- seekers -->
+            <section id="sc-seekers">
+                <div class="line">
+                    <picture>
+                        <source media="(max-width: 767px)" srcset="{{asset('images/service/service_seekers_line_sp.png')}}">
+                        <source media="(min-width: 768px)" srcset="{{asset('images/service/service_seekers_line_pc.png')}}">
+                        <img class="sizes" src="{{asset('images/service/service_seekers_line_pc.png')}}" alt="">
+                    </picture>
+                </div>
+                <div class="inner">
+                    <h2 class="section-title">
+                        <img class="sizes" src="{{asset('images/service/service_title.png')}}" alt="">
+                        求職者向けお仕事紹介サービス
+                    </h2>
+                </div>
+                <div class="seekers-inner">
+                    <div class="content">
+                        <p class="text-bold">個性や強みを活かして<br />自己成長につながるお仕事を紹介します</p>
+                        <p class="text">求職者のみなさまへ。私たちSASAKI CONNECTは製造業に特化したお仕事紹介または派遣サービスをご紹介しています。製造業の中でも大手・成長産業分野に強い当社が、正社員就職やキャリア転職など、あなたのステップアップを応援いたします。</p>
+                    </div>
+                    <div class="img">
+                        <picture>
+                            <source media="(max-width: 767px)" srcset="{{asset('images/service/service_seekers_01_sp.jpg')}}">
+                            <source media="(min-width: 768px)" srcset="{{asset('images/service/service_seekers_01_pc.jpg')}}">
+                            <img class="sizes" src="{{asset('images/service/service_seekers_01_pc.jpg')}}" alt="">
+                        </picture>
+                    </div>
+                    <div class="box">
+                        <h3 class="box-title">当社でお仕事をするメリット</h3>
+                        <ul class="list">
+                            <li class="text">安定の大手製造業で仕事ができます</li>
+                            <li class="text">スキルアップができます</li>
+                            <li class="text">付加価値の高い業務経験でキャリアアップも</li>
+                        </ul>
+                    </div>
+                </div>
+            </section>
+
+            <!-- recruit -->
+            <section id="sc-recruit">
+                <div class="recruit-inner">
+                    <h2 class="section-title">
+                        <img class="sizes" src="{{asset('images/service/service_title.png')}}" alt="">
+                        採用・お仕事情報
+                    </h2>
+                    <div class="grid">
+                        <p class="text">詳しくはこちらの<br class="sp-br" />お仕事をお探しの方へのページをご覧ください</p>
+                        <dl class="box">
+                            <dt class="box-title"><span>＂</span>一段上の<span>＂</span>お仕事で確かなスキルを</dt>
+                            <dd class="box-text">SASAKI CONNECTで<br class="sp-br" />安定・ハイスキルの仕事を見つけよう！</dd>
+                        </dl>
+                        <div class="img">
+                            <picture>
+                                <source media="(max-width: 767px)" srcset="{{asset('images/service/service_recruit_sp.jpg')}}">
+                                <source media="(min-width: 768px)" srcset="{{asset('images/service/service_recruit_pc.jpg')}}">
+                                <img class="sizes" src="{{asset('images/service/service_recruit_pc.jpg')}}" alt="">
+                            </picture>
+                        </div>
+                        <a href="#" class="link">詳しくはこちら</a>
+                    </div>
+                </div>
+            </section>
+        </div> 
+
+
+
+
             
             <x-contactFooter />    
 
